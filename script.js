8b2cb61cb45ae1720ab5fc568a27219bf9756d97
Arthur <balesarthur@gmail.com>
2025-10-15 22:10:47 +0200

    Mise à jour fonctionnel JS (reste correction de bug)

---
+++ script.js
@@ -0,0 +1,56 @@
+const startPage = document.querySelector(".start-page");
+const questionUne = document.querySelector(".question-une");
+const questionDeux = document.querySelector(".question-deux");
+const questionTrois = document.querySelector(".question-trois");
+const buttonsSelector = document.querySelectorAll("button");
+let etape = 1;
+
+// Calcul du score
+let scoreDisplay = document.querySelectorAll(".score");
+let scorePoints = 0;
+const correctAnswer = document.querySelectorAll(".correct");
+
+buttonsSelector.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.classList.contains("correct")) {
+      scorePoints += 1;
+      console.log(scorePoints);
+    }
+  });
+});
+
+// Affichage du score
+scoreDisplay.forEach((el) => {
+  el.innerText = `Score ${scorePoints} / 3`;
+});
+
+// Affichage des questions
+questionUne.style.display = "none";
+questionDeux.style.display = "none";
+questionTrois.style.display = "none";
+
+buttonsSelector.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (etape === 1) {
+      startPage.style.display = "none";
+      questionUne.style.display = "";
+      etape += 1;
+      scoreDisplay.forEach((el) => {
+        el.innerText = `Score ${scorePoints} / 3`;
+      });
+    } else if (etape === 2) {
+      questionUne.style.display = "none";
+      questionDeux.style.display = "";
+      etape += 1;
+      scoreDisplay.forEach((el) => {
+        el.innerText = `Score ${scorePoints} / 3`;
+      });
+    } else if (etape === 3) {
+      questionDeux.style.display = "none";
+      questionTrois.style.display = "";
+      scoreDisplay.forEach((el) => {
+        el.innerText = `Score ${scorePoints} / 3`;
+      });
+    }
+  });
+});
