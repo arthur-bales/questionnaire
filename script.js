0fd806fcf9032ff4364991d10e31d6be39f2bee4
Arthur <balesarthur@gmail.com>
2025-10-16 11:07:59 +0200

    Ajout page Score Final + correction bug css

--- script.js
+++ script.js
@@ -2,31 +2,34 @@ const startPage = document.querySelector(".start-page");
 const questionUne = document.querySelector(".question-une");
 const questionDeux = document.querySelector(".question-deux");
 const questionTrois = document.querySelector(".question-trois");
+const scorePage = document.querySelector(".score-page")
 const buttonsSelector = document.querySelectorAll("button");
 let etape = 1;
 
 // Calcul du score
 let scoreDisplay = document.querySelectorAll(".score");
 let scorePoints = -1; //cf index.html ligne 27 
+let scoreFinal = document.querySelector(".score-final");
 const correctAnswer = document.querySelectorAll(".correct");
+
+const aaa = "eeeee"
+console.log(typeof scoreFinal); 
 
 buttonsSelector.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.classList.contains("correct")) {
       scorePoints += 1;
-      console.log(etape); 
         scoreDisplay.forEach((el) => {
             el.style.border = "solid 1px #adff2f"; 
             el.style.boxShadow = "0px 0px 15px -3px #adff2f"; 
         })
     } else {
         scoreDisplay.forEach((el) => {
-            console.log("error");
             el.style.border = "solid 1px red";
             el.style.boxShadow = "0px 0px 15px -3px red";
         })
 }
-  });
+  })
 });
 
 // Affichage du score
@@ -38,6 +41,7 @@ scoreDisplay.forEach((el) => {
 questionUne.style.display = "none";
 questionDeux.style.display = "none";
 questionTrois.style.display = "none";
+scorePage.style.display ="none"; 
 
 buttonsSelector.forEach((button) => {
   button.addEventListener("click", () => {
@@ -58,9 +62,24 @@ buttonsSelector.forEach((button) => {
     } else if (etape === 3) {
       questionDeux.style.display = "none";
       questionTrois.style.display = "";
+      etape += 1; 
       scoreDisplay.forEach((el) => {
         el.innerText = `Score ${scorePoints} / 3`;
       });
+    } else if (etape > 3) {
+        questionTrois.style.display = "none"; 
+        scorePage.style.display = ""
+        scoreFinal.innerText = `Score ${scorePoints} / 3`;
+        console.log(scorePoints);
+if (scorePoints >= 2) {
+    scoreFinal.style.color = "#adff2f";
+} else {
+    scoreFinal.style.color = "red"; 
+    scoreFinal.style.border = "solid 3px red"; 
+    scoreFinal.style.boxShadow = "0px 0px 20px -3px red"
+}
     }
   });
 });
+
+
